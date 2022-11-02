856e6f1a03933495f84d54efc55cc4a76c6099e6
JakubNowek <jakubbnwek@gmail.com>
2022-11-02 16:45:03 +0100

    Added example posts displaying

--- flaskfirstapp.py
+++ flaskfirstapp.py
@@ -3,19 +3,34 @@
 # restartować serwera, żeby je zobaczyć - tak samo działa jak się napisze na dole w app.run(debug=True) ale to działa
 # w uruchamianiu w pythonie - py flaskfirstapp.py
 # ustawianie zmiennych środowiskowych działa tylko w danej sesji terminala
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# dummy data, which pretends a database response with posts
+posts = [
+    {
+        'author': 'Tomasz Strzelba',
+        'title': 'Blog Post 1',
+        'content': 'First post content - here i would place some description of the page functionalities or whatever',
+        'date': 'November 2, 2022'
+    },
+    {
+        'author': 'Katarzyna Katastrofa',
+        'title': 'Blog Post 2',
+        'content': 'Second post content - it may be just another html site',
+        'date': 'November 4, 2022'
+    }
+]
 
 @app.route('/')
-def index():  # put application's code here
-    return "<h1>Homo page</h1>"
+def index():
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1 style='color:red'>About page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
